fdfeda9e07390c9b7c199ea5d791b0da046dc9e0
Daan de Waard <daan.de.waard@hz.nl>
2020-11-03 20:17:00 +0100

    Extracted constants for magic numbers and strings

--- src/Game.ts
+++ src/Game.ts
@@ -13,20 +13,43 @@ class Game {
 
     private playerPositionX: number;
 
+    public static readonly WINDOW_WIDTH_OFFSET = 1;
+    public static readonly WINDOW_HEIGHT_OFFSET = 4;
+
+    public static readonly GRAVITY = 0.98;
+    public static readonly FULL_CIRCLE = 2 * Math.PI;
+
+    public static readonly MIN_BALL_RADIUS = 25;
+    public static readonly BALL_RADIUS_SCATTER = 25;
+    public static readonly MIN_BALL_X_SPEED = -50;
+    public static readonly BALL_X_SPEED_SCATTER = 100;
+    public static readonly MIN_BALL_Y_SPEED = 0;
+    public static readonly BALL_Y_SPEED_SCATTER = 0;
+    public static readonly BALL_Y_POSITION_AREA = 0.2;
+    public static readonly BALL_COLOR = 'blue';
+
+    public static readonly PLAYER_BALL_RADIUS = 50;
+    public static readonly PLAYER_COLOR = 'red';
+
     public constructor(canvas: HTMLElement) {
         this.canvas = <HTMLCanvasElement>canvas;
         
-        // Resize the canvas to full window size
-        this.canvas.width = window.innerWidth - 1;
-        this.canvas.height = window.innerHeight - 4; 
+        // Resize the canvas to full window size minus some small offset
+        this.canvas.width = window.innerWidth - Game.WINDOW_WIDTH_OFFSET;
+        this.canvas.height = window.innerHeight - Game.WINDOW_HEIGHT_OFFSET; 
         
         // Spawn a Ball
-        this.ballRadius = 25 + 25 * Math.random();
-        this.ballSpeedX = -50 + 100 * Math.random();
-        this.ballSpeedY = 0;
+        this.ballRadius = Game.MIN_BALL_RADIUS + 
+            Game.BALL_RADIUS_SCATTER * Math.random();
+        this.ballSpeedX = Game.MIN_BALL_X_SPEED + 
+            Game.BALL_X_SPEED_SCATTER * Math.random();
+        this.ballSpeedY = Game.MIN_BALL_Y_SPEED + 
+            Game.BALL_Y_SPEED_SCATTER * Math.random();
         this.ballPositionX = this.ballRadius +  
             (this.canvas.width - 2 * this.ballRadius)*Math.random();
-        this.ballPositionY = this.canvas.height * 0.8 + this.canvas.height * 0.2 * Math.random();
+        this.ballPositionY = 
+            this.canvas.height * (1 - Game.BALL_Y_POSITION_AREA) + 
+            this.canvas.height * Game.BALL_Y_POSITION_AREA * Math.random();
         
         // Set the player at the center
         this.playerPositionX = this.canvas.width / 2;
@@ -64,12 +87,12 @@ class Game {
         // Some physics here: the y-portion of the speed changes due to gravity
         // Formula: Vt = V0 + gt
         // 9.8 is the gravitational constant and time=1
-        this.ballSpeedY -= 0.98;
+        this.ballSpeedY -= Game.GRAVITY;
         // Calculate new X and Y parts of the position 
         // Formula: S = v*t
         this.ballPositionX += this.ballSpeedX;
         // Formula: S=v0*t + 0.5*g*t^2
-        this.ballPositionY += this.ballSpeedY + 0.5 * 0.98;
+        this.ballPositionY += this.ballSpeedY + 0.5 * Game.GRAVITY;
     }
 
     /**
@@ -105,7 +128,7 @@ class Game {
         // theorem
         const distance = Math.sqrt(distX * distX + distY * distY);
         // Collides is distance <= sum of radii of both circles
-        const gameover = distance <= (this.ballRadius + 50);
+        const gameover = distance <= (this.ballRadius + Game.PLAYER_BALL_RADIUS);
         return gameover;
     }
 
@@ -119,18 +142,20 @@ class Game {
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
         // Draw the player
-        ctx.fillStyle = 'red';
+        ctx.fillStyle = Game.PLAYER_COLOR;
         ctx.beginPath();
-        const playerPositionY = this.canvas.height - 50;
-        ctx.ellipse(this.playerPositionX, playerPositionY, 50, 50, 0, 0, 2 * Math.PI);
+        const playerPositionY = this.canvas.height - Game.PLAYER_BALL_RADIUS;
+        ctx.ellipse(this.playerPositionX, playerPositionY, 
+            Game.PLAYER_BALL_RADIUS, Game.PLAYER_BALL_RADIUS, 0, 0, Game.FULL_CIRCLE);
         ctx.fill();
 
         // Draw the ball
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = Game.BALL_COLOR;
         ctx.beginPath();
         // reverse height, so the ball falls down
         const y = this.canvas.height - this.ballPositionY;
-        ctx.ellipse(this.ballPositionX, y, this.ballRadius, this.ballRadius, 0, 0, 2 * Math.PI);
+        ctx.ellipse(this.ballPositionX, y, this.ballRadius, this.ballRadius, 
+            0, 0, Game.FULL_CIRCLE);
         ctx.fill();
     }
 }
